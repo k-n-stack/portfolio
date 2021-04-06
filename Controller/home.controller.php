597d71c26349b3a1d373e49018da2d1f9621a5ff
nico-php-coder <khin.nicolas@gmail.com>
2021-04-06 13:09:55 +0200

    continue...

--- Controller/home.controller.php
+++ Controller/home.controller.php
@@ -1,3 +1,11 @@
 <?php
 
 echo "home controller";
+
+if(empty($_SESSION['user'])) {
+    include_once("View/home.php");
+} elseif($_SESSION['user']->login() === true) {
+    include_once("View/homeLogin.php");
+} else {
+    include_once("View/loginFail.php");
+}
